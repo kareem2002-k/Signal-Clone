7a13fc68a5c8dcc6c84a562bb42328ca13f4b4bf
kareem2002-k <kareemsamir166@gmail.com>
2023-06-17 12:26:03 +0300

    Adjusting Dependencies

--- Signal/App.js
+++ Signal/App.js
@@ -1,12 +1,21 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from "./Home";
+
 
 export default function App() {
+
+  const Stack = createStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    
+    </NavigationContainer>
   );
 }
 
